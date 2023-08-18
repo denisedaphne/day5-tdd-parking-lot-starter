fa9cab83f74460252173594f9a6921261c17cc63
Daphne Balane <balanedaphne20@gmail.com>
2023-08-18 13:58:27 +0800

    test: added should_return_parked_car_with_ticket_when_fetch_given_parking_lot

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -18,4 +18,18 @@ public class ParkingLotTest {
         Assertions.assertNotNull(parkingTicket);
     }
 
+    @Test
+    void should_return_parked_car_with_ticket_when_fetch_given_parking_lot() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        //when
+        ParkingTicket parkingTicket = parkingLot.parkCar(car);
+        Car fetchCar = parkingLot.fetchCar(parkingTicket);
+
+        //then
+        Assertions.assertEquals(car, fetchCar);
+    }
+
 }
