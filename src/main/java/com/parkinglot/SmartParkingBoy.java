582c45eaa1867780318229ac698823278fe6eb4a
Daphne Balane <balanedaphne20@gmail.com>
2023-08-19 15:03:42 +0800

    feat: added fetch method implementation in SmartParkingBoy class

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -2,8 +2,10 @@ package com.parkinglot;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import com.parkinglot.exception.NoAvailablePositionException;
+import com.parkinglot.exception.UnrecognizedTicketException;
 
 public class SmartParkingBoy {
     private final List<ParkingLot> parkingLots;
@@ -17,5 +19,18 @@ public class SmartParkingBoy {
                 .max(Comparator.comparingInt(ParkingLot::getAvailableCapacity))
                 .orElseThrow(NoAvailablePositionException::new)
                 .parkCar(car);
+    }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return parkingLots.stream()
+                .flatMap(parkingLot -> {
+                    try {
+                        return Stream.of(parkingLot.fetchCar(parkingTicket));
+                    } catch (UnrecognizedTicketException ignored) {
+                        return Stream.empty();
+                    }
+                })
+                .findFirst()
+                .orElseThrow();
     }
 }
