fcb4acf70b748cec393a93f3f517d4437cb2871f
Daphne Balane <balanedaphne20@gmail.com>
2023-08-19 16:28:38 +0800

    feat: added implementation for unrecognized ticket exception in SuperParkingBoy class

--- src/main/java/com/parkinglot/SuperParkingBoy.java
+++ src/main/java/com/parkinglot/SuperParkingBoy.java
@@ -31,6 +31,6 @@ public class SuperParkingBoy {
                     }
                 })
                 .findFirst()
-                .orElseThrow();
+                .orElseThrow(UnrecognizedTicketException::new);
     }
 }
