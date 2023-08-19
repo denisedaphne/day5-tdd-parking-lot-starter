0f169e3ca893e448e876b7682f507657cf22119e
Daphne Balane <balanedaphne20@gmail.com>
2023-08-19 13:57:23 +0800

    test: added should_park_to_second_parking_lot_when_park_given_standard_parking_boy_and_two_parking_lots_first_parking_lot_is_full_and_car

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class StandardParkingBoyTest {
     ParkingLot firstParkingLot = new ParkingLot();
@@ -23,5 +23,21 @@ public class StandardParkingBoyTest {
         Assertions.assertNotNull(parkingTicket);
         Assertions.assertEquals(9, firstParkingLot.getAvailableCapacity());
         Assertions.assertEquals(10, secondParkingLot.getAvailableCapacity());
+    }
+
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_standard_parking_boy_and_two_parking_lots_first_parking_lot_is_full_and_car() {
+        //Given
+        List<Car> cars = IntStream.range(0, 10)
+                .mapToObj(i -> new Car())
+                .collect(Collectors.toList());
+
+        cars.forEach(car -> firstParkingLot.parkCar(car));
+        //When
+        ParkingTicket parkingTicket = standardParkingBoy.park(new Car());
+        //Then
+        Assertions.assertNotNull(parkingTicket);
+        Assertions.assertEquals(0, firstParkingLot.getAvailableCapacity());
+        Assertions.assertEquals(9, secondParkingLot.getAvailableCapacity());
     }
 }
