921b4b59d6bce280d9f26e419172283f831f2569
Daphne Balane <balanedaphne20@gmail.com>
2023-08-19 15:51:43 +0800

    test: added should_park_to_second_parking_lot_when_park_given_super_parking_boy_and_two_parking_lots_first_parking_lot_is_full_and_car

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -3,6 +3,8 @@ package com.parkinglot;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -27,4 +29,22 @@ public class SuperParkingBoyTest {
         assertEquals(9, firstParkingLot.getAvailableCapacity());
         assertEquals(20, secondParkingLot.getAvailableCapacity());
     }
+
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_super_parking_boy_and_two_parking_lots_first_parking_lot_is_full_and_car() {
+        //Given
+        List<Car> cars = IntStream.range(0, 10)
+                .mapToObj(i -> new Car())
+                .collect(Collectors.toList());
+
+        cars.forEach(firstParkingLot::parkCar);
+        //When
+        ParkingTicket parkingTicket = superParkingBoy.park(new Car());
+        //Then
+        assertNotNull(parkingTicket);
+        assertEquals(0, firstParkingLot.getAvailableCapacity());
+        assertEquals(19, secondParkingLot.getAvailableCapacity());
+    }
+
+    
 }
