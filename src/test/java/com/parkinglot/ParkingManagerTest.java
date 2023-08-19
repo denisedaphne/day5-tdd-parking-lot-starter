3a802e37c0d07b050550f800ad129648e0760376
Daphne Balane <balanedaphne20@gmail.com>
2023-08-19 18:26:20 +0800

    test: added should_return_car_when_park_given_parking_manager_use_parking_boy_without_available_parking_lots

--- src/test/java/com/parkinglot/ParkingManagerTest.java
+++ src/test/java/com/parkinglot/ParkingManagerTest.java
@@ -1,5 +1,7 @@
 package com.parkinglot;
 
+import com.parkinglot.exception.NoAvailablePositionException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -34,5 +36,18 @@ public class ParkingManagerTest {
         assertEquals(car, fetchedCar);
     }
 
+    @Test
+    void should_return_car_when_park_given_parking_manager_use_parking_boy_without_available_parking_lots() {
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.setCapacity(0);
+
+        Car car = new Car();
+
+        ParkingTicket ticket = ParkingManager.parkCar(car, ParkingManager.getFirstAvailableParkingBoy());
+        assertNotNull(ticket);
+
+        NoAvailablePositionException noAvailablePositionException = Assertions.assertThrows(NoAvailablePositionException.class, () -> parkingLot.parkCar(new Car()));
+        assertEquals("No available position", noAvailablePositionException.getMessage());
+    }
 
 }
